296c035d3dbdbbbb19e8c94485d4e0bf3ede1eb2
hamza-smh <hamza.smh2000@gmail.com>
2025-01-20 23:32:54 +0500

    Difficulty Test F

--- block.test.js
+++ block.test.js
@@ -7,56 +7,78 @@ describe('Block', () => {
   const hash = 'h-data'
   const lastHash = 'lh-date'
   const data = ['d-data2', 'd-data']
-  const block = new Block({ timestamp, hash, lastHash, data })
+  const nonce = 1
+  const difficulty = 1
+  const block = new Block({
+    timestamp,
+    hash,
+    lastHash,
+    data,
+    nonce,
+    difficulty
+  })
 
   it('has a timestamp,a hash , a lastHash ,and a data property', () => {
     expect(block.timestamp).toEqual(timestamp)
     expect(block.lastHash).toEqual(lastHash)
     expect(block.hash).toEqual(hash)
     expect(block.data).toEqual(data)
-  });
+    expect(block.nonce).toEqual(nonce)
+    expect(block.difficulty).toEqual(difficulty)
+  })
 
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis()
 
+    //  console.log("genesis", genesisBlock);
 
-  describe('genesis()',()=>{
-    const genesisBlock = Block.genesis();
-
-  //  console.log("genesis", genesisBlock);
-
-    it('returns a valid block instance',()=>{
-        expect(genesisBlock instanceof Block).toBe(true);
-    });
-
-    it('returns genesis data',()=>{
-        expect(genesisBlock).toEqual(GENESIS_DATA)
+    it('returns a valid block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true)
     })
-});
 
-    describe('mineBlock()', () => { 
-        const lastBlock =  Block.genesis();
-        const data = 'Mined Data';
-        const minedBlock = Block.mineBlock({lastBlock,data});
-
-        it('returns a Block instance',()=>{
-            expect(minedBlock instanceof Block).toBe(true)
-        });
-
-        it('sets the `lastHash` to the `hash` of the lastBlock',()=>{
-            expect(minedBlock.lastHash).toEqual(lastBlock.hash)
-        });
-
-        it('set the data',()=>{
-            expect(minedBlock.data).toEqual(data)
-        });
-
-        it('sets the timestamp',()=>{
-            //expect(typeof minedBlock.timestamp).toBe('string')
-            expect(minedBlock.timestamp).not.toEqual(undefined);
-        });
-
-        it('creates a SHA-256 `hash` based on proper inputs',()=>{
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
-        });
-
+    it('returns genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA)
     })
+  })
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis()
+    const data = 'Mined Data'
+    const minedBlock = Block.mineBlock({ lastBlock, data })
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true)
+    })
+
+    it('sets the `lastHash` to the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash)
+    })
+
+    it('set the data', () => {
+      expect(minedBlock.data).toEqual(data)
+    })
+
+    it('sets the timestamp', () => {
+      //expect(typeof minedBlock.timestamp).toBe('string')
+      expect(minedBlock.timestamp).not.toEqual(undefined)
+    })
+
+    it('creates a SHA-256 `hash` based on proper inputs', () => {
+      expect(minedBlock.hash)
+        .toEqual(
+            cryptoHash(
+              minedBlock.timestamp,
+              minedBlock.nonce,
+              minedBlock.difficulty,
+              lastBlock.hash,
+              data
+            )
+        )
+    })
+
+    it('sets a `hash` that matches the difficulty',()=>{
+        expect(minedBlock.hash.subsring(0,minedBlock.difficulty))        
+            .toEqual('0'.repeat(minedBlock.difficulty))
+    })
+  })
 })
