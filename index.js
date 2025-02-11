f0672fdbd0a9e00be033a4f08542b99d43fefdcf
hamza-smh <hamza.smh2000@gmail.com>
2025-02-11 15:38:42 +0500

    Sync Chain on peer node

--- index.js
+++ index.js
@@ -67,12 +67,17 @@
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const request = require('request')
 const Blockchain = require('./blockchain')
 const PubSub = require('./pubsub')
 
 const app = express()
 const blockchain = new Blockchain()
-const pubsub = new PubSub({blockchain})
+const pubsub = new PubSub({blockchain});
+
+const DEFAULT_PORT = 3000
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 setTimeout(()=> pubsub.broadcastChain(), 1000)
 
@@ -91,7 +96,17 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks')
 })
 
-const DEFAULT_PORT = 3000;
+const syncChains = () =>{
+    request({url:`${ROOT_NODE_ADDRESS}/api/blocks`}, (error,response, body)=>{
+        if(!error && response.statusCode===200){
+            const rootChain = JSON.parse(body);
+
+            console.log("Replace chain on a sync with ",rootChain)
+            blockchain.replaceChain(rootChain)
+        }
+    })
+}
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true'){
@@ -101,8 +116,8 @@ if(process.env.GENERATE_PEER_PORT === 'true'){
 const PORT = PEER_PORT || DEFAULT_PORT; 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`);
+
+  syncChains()
 })
 
-// Broadcasting after a short delay to ensure proper connection
-setTimeout(() => pubsub.broadcastChain(), 1000)
