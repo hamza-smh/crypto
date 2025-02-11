fd8f962f02a5016a65f571fb7e4f7bc9425db084
hamza-smh <hamza.smh2000@gmail.com>
2025-02-11 15:53:16 +0500

    remove redundancy

--- index.js
+++ index.js
@@ -1,70 +1,3 @@
-// const bodyParser = require('body-parser')
-// const express = require('express');
-// const request = require('request');
-// const Blockchain = require('./blockchain')
-// const PubSub = require('./pubsub');
-
-
-
-// const app = express();
-// const blockchain = new Blockchain();
-// //const pubsub = new PubSub({blockchain}); 
-
-// //const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
-
-// setTimeout(() => pubsub.broadcastChain(), 1000);
-
-// app.use(bodyParser.json())
-
-// app.get('/api/blocks', (req,res)=>{
-//     res.json(blockchain.chain);
-// })
-
-// app.post('/api/mine',(req,res)=>{
-//     const {data} = req.body;
-//     blockchain.addBlock({data});
-//     pubsub.broadcastChain();
-//     res.redirect('/api/blocks');
-// })
-
-// const PORT = 3000
-
-// //let PEER_PORT;
-
-// // if(process.env.GENERATE_PEER_PORT === 'true'){
-// //     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() *1000);
-// // }
-
-// //const PORT = PEER_PORT || DEFAULT_PORT;
-
-// app.listen(PORT,()=>{
-//     console.log(`Server is running on port localhost:${PORT}`);  
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const bodyParser = require('body-parser')
 const express = require('express')
 const request = require('request')
@@ -73,13 +6,13 @@ const PubSub = require('./pubsub')
 
 const app = express()
 const blockchain = new Blockchain()
-const pubsub = new PubSub({blockchain});
+const pubsub = new PubSub({ blockchain })
 
 const DEFAULT_PORT = 3000
 
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
-setTimeout(()=> pubsub.broadcastChain(), 1000)
+//setTimeout(()=> pubsub.broadcastChain(), 1000)
 
 app.use(bodyParser.json())
 
@@ -91,33 +24,37 @@ app.post('/api/mine', (req, res) => {
   const { data } = req.body
 
   blockchain.addBlock({ data })
-  pubsub.broadcastChain();
+  pubsub.broadcastChain()
 
   res.redirect('/api/blocks')
 })
 
-const syncChains = () =>{
-    request({url:`${ROOT_NODE_ADDRESS}/api/blocks`}, (error,response, body)=>{
-        if(!error && response.statusCode===200){
-            const rootChain = JSON.parse(body);
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body)
 
-            console.log("Replace chain on a sync with ",rootChain)
-            blockchain.replaceChain(rootChain)
-        }
-    })
+        console.log('Replace chain on a sync with ', rootChain)
+        blockchain.replaceChain(rootChain)
+      }
+    }
+  )
 }
 
-let PEER_PORT;
+let PEER_PORT
 
-if(process.env.GENERATE_PEER_PORT === 'true'){
-    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
 }
 
-const PORT = PEER_PORT || DEFAULT_PORT; 
+const PORT = PEER_PORT || DEFAULT_PORT
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`)
 
-  syncChains()
+  if (PORT !== DEFAULT_PORT) {
+    syncChains()
+  }
 })
-
--- pubsub.js
+++ pubsub.js
@@ -1,3 +1,66 @@
+//Implementing Pubnub
+
+const PubNub = require('pubnub')
+const credentials = {
+  publishKey: 'pub-c-b2fb93f3-2832-4fe5-b6aa-2dbddb648e8d',
+  subscribeKey: 'sub-c-33e33841-b48a-41fe-bcb7-470c00bbca55',
+  secretKey: 'sec-c-MTdmMzdiYWUtZmMwMi00NGRiLTk3MmUtMzcwMmZmZjkyMTM2'
+}
+
+const CHANNELS = {
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN'
+}
+
+class PubSub {
+  constructor ({ blockchain }) {
+    this.blockchain = blockchain
+    this.pubnub = new PubNub(credentials)
+
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) })
+
+    this.pubnub.addListener(this.listener())
+  }
+
+  listener () {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject
+
+        console.log(`Received message on channel ${channel}: ${message}`)
+        const parsedMessage = JSON.parse(message)
+        if (channel === CHANNELS.BLOCKCHAIN) {
+          this.blockchain.replaceChain(parsedMessage)
+        }
+      }
+    }
+  }
+
+  publish ({ channel, message }) {
+    this.pubnub.unsubscribe(channel, () => {
+      this.pubnub.publish(channel, message, () => {
+        this.pubnub.subscribe(channel)
+      })
+    })
+  }
+
+  // publish({channel,message}){
+  //   this.pubnub.publish({channel,message});
+  // }
+
+  broadcastChain () {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    })
+  }
+}
+//  const testPubSub = new PubSub();
+//  testPubSub.publish({channel:CHANNELS.TEST,message:"Hi Bro"})
+
+module.exports = PubSub
+
+// Redis Implementation
 // const redis = require('redis')
 
 // const CHANNELS = {
@@ -53,57 +116,3 @@
 // // setTimeout(()=>testPubSub.publisher.publish(CHANNELS.TEST,"Yay !"),1000)
 
 // module.exports = PubSub;
-
-//Implementing Pubnub
-
-const PubNub = require('pubnub')
-const credentials = {
-  publishKey: 'pub-c-b2fb93f3-2832-4fe5-b6aa-2dbddb648e8d',
-  subscribeKey: 'sub-c-33e33841-b48a-41fe-bcb7-470c00bbca55',
-  secretKey: 'sec-c-MTdmMzdiYWUtZmMwMi00NGRiLTk3MmUtMzcwMmZmZjkyMTM2'
-}
-
-const CHANNELS = {
-  TEST: 'TEST',
-  BLOCKCHAIN: 'BLOCKCHAIN'
-}
-
-class PubSub {
-  constructor ({blockchain}) {
-    this.blockchain = blockchain
-    this.pubnub = new PubNub(credentials)
-
-    this.pubnub.subscribe({ channels: Object.values(CHANNELS) })
-
-    this.pubnub.addListener(this.listener())
-  }
-
-  listener () {
-    return {
-      message: messageObject => {
-        const { channel, message } = messageObject
-
-        console.log(`Received message on channel ${channel}: ${message}`)
-        const parsedMessage = JSON.parse(message);
-        if(channel === CHANNELS.BLOCKCHAIN){
-          this.blockchain.replaceChain(parsedMessage);
-        }
-      }
-    }
-  }
-
-  publish({channel,message}){
-    this.pubnub.publish({channel,message});
-  }
-
-  broadcastChain(){
-    this.publish({
-          channel: CHANNELS.BLOCKCHAIN,
-          message: JSON.stringify(this.blockchain.chain)
-        })
-  }
-}
-//  const testPubSub = new PubSub();
-//  testPubSub.publish({channel:CHANNELS.TEST,message:"Hi Bro"})
-
-module.exports = PubSub
