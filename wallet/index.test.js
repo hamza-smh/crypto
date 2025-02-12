82216de6eaef9c49aa6b3e14b4438da1ec15cbde
hamza-smh <hamza.smh2000@gmail.com>
2025-02-12 15:46:24 +0500

    Verify Signature and sign data

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,19 +1,44 @@
-const Wallet = require('./index');
+const Wallet = require('./index')
+const { verifySignature } = require('../util')
 
 describe('Wallet', () => {
-    let wallet;
+  let wallet
 
-    beforeEach(() => {
-        wallet = new Wallet();
-    });
+  beforeEach(() => {
+    wallet = new Wallet()
+  })
 
-    it('has a `balance`',()=>{
-        expect(wallet).toHaveProperty('balance')
-    });
+  it('has a `balance`', () => {
+    expect(wallet).toHaveProperty('balance')
+  })
 
-    it('has a `publicKey`',()=>{
-        //console.log("Public Key: ",wallet.publicKey)
-        expect(wallet).toHaveProperty('publicKey')
-    });
+  it('has a `publicKey`', () => {
+    //console.log("Public Key: ",wallet.publicKey)
+    expect(wallet).toHaveProperty('publicKey')
+  })
 
-});
+  describe('signing data', () => {
+    const data = 'foooobar'
+
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data)
+        })
+      ).toBe(true);
+    })
+
+    it('does not verify an invalid signature', () => {
+        expect(
+            verifySignature({
+              publicKey: wallet.publicKey,
+              data,
+              signature: new Wallet().sign(data)
+            })
+        ).toBe(false);
+
+    })
+  })
+})
