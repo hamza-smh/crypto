5937eb5d5472a54edfbcfe54f4dff43ab19b90ba
hamza-smh <hamza.smh2000@gmail.com>
2025-02-12 15:12:36 +0500

    Add public Key

--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,14 @@
-class Wallet{}
+const {STARTING_BALANCE} =require('../config')
+const { ec } = require('../util')
+class Wallet{
+    constructor(){
+       this.balance= STARTING_BALANCE; 
+
+       const keyPair = ec.genKeyPair();
+
+       this.publicKey = keyPair.getPublic().encode('hex');
+//       this.privateKey = keyPair.getPrivate();
+    }
+}
 
 module.exports = Wallet;
