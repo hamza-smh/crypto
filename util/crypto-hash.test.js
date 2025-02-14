0ea041b1a1f790ffb3d5d54529814e89213f5acc
hamza-smh <hamza.smh2000@gmail.com>
2025-02-14 23:14:33 +0500

    improve vryptoHash by stringify

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -4,11 +4,19 @@ describe('cryptoHash()',()=>{
 
     it('generates a SHA-256 hashed output',()=>{
         expect(cryptoHash('My name is Hamza'))
-            .toEqual('168f60b7983dd2c75aa17ec7600b95d8c4fa540a4fc86785a657eb6d9aa8ea92')
+            .toEqual('5eabc32663655c0ef64d43b5a4bb539d8a0cc8d50558308aa01df67fe235192e')
     });
 
     it('produces the same hash with same arguments in any order',()=>{
         expect(cryptoHash('One','Two','Three')).toEqual(cryptoHash('Three','One','Two'));
+    })
+
+    it('produces a unique hash when properties have changed on an input',()=>{
+        const foo={};
+        const originalHash = cryptoHash(foo);
+        foo['a'] = 'a';
+
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
     })
 
 })
