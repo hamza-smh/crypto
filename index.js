d1790cc942381fe255f6953893eb0a6ca1833183
hamza-smh <hamza.smh2000@gmail.com>
2025-02-20 12:45:56 +0500

    Transaction peer broadcast

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const blockchain = new Blockchain();
 const transactionPool  = new TransactionPool()
 const wallet = new Wallet()
-const pubsub = new PubSub({ blockchain })
+const pubsub = new PubSub({ blockchain, transactionPool, wallet })
 
 const DEFAULT_PORT = 3000
 
@@ -50,7 +50,8 @@ app.post('/api/transact', (req, res) => {
   }
 
   transactionPool.setTransaction(transaction);
-
+  pubsub.broadcastTransaction(transaction);
+   
   res.json({type:'success',transaction});
 })
 
