b9bea8174aad644f411afeb1d725403f0463a94c
hamza-smh <hamza.smh2000@gmail.com>
2025-02-28 16:15:39 +0500

    WalletInfo Fetch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React,{Component} from 'react'
 
 class App extends Component {
+
+    state={walletInfo:{}}
+
+    componentDidMount(){
+        fetch('http://localhost:3000/api/wallet-info')
+        .then(response => response.json())
+        .then(json=>this.setState({walletInfo:json}));
+    }
     render(){
+        const {address, balance} = this.state.walletInfo;
+
         return (
             <div>
-                <h1>Welcome to Cryptochain in React...</h1>
+                <div>
+                    <h1>Welcome to Cryptochain in React...</h1>
+                </div>
+                <div>Address:{address}</div>
+                <div>Balance:{balance}</div>
             </div>
         )
     }
