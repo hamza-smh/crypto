76c51451b0baa1eef24103139e1da8ea431840bb
hamza-smh <hamza.smh2000@gmail.com>
2025-02-28 11:39:27 +0500

    Frontend dev ok

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+
+
+
+
+
+
+
+
+// "test": "jest --watchAll",
+//     "start": "node index.js",
+//     "dev": "nodemon index.js",
+//     "dev-peer": "cross-env GENERATE_PEER_PORT='true' nodemon index.js",
+//     "start-redis": "redis-server --daemonize yes",
+//     "build-client": "parcel build client/src/index.html --out-dir client/dist"
